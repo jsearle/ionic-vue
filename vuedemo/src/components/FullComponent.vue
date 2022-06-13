97a4d677909a8886eafe363659ca3d27d99d102d
Juan Searle (Casa) <jsearle@gmail.com>
2022-06-13 18:24:04 +0200

    Comentarios para los alumnos en el componente FullComponent

--- vuedemo/src/components/FullComponent.vue
+++ vuedemo/src/components/FullComponent.vue
@@ -2,30 +2,33 @@
   <h1>Componente plantilla</h1>
   <p>
     Texto: {{texto}}. Nombre {{nombre}}
-    </p>
+  </p>
   <p>
     Numero: {{numero}} 
-    </p>
-    <p>
-      Composition API<br/>
-      <strong>{{ texto2 }}</strong>
-    </p>
-    <p>{{mouse}}</p>
-    <input type="text" v-model="nombre" />
+  </p>
+  <p>
+    Composition API<br/>
+    <strong>{{ texto2 }}</strong>
+  </p>
+  <p>{{mouse}}</p>
+  <input type="text" v-model="nombre" />
   <button @click="modificarPadre">Modificar padre</button>
   <button @click="cambioProfundo">Botón pulsado</button>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, reactive } from 'vue'
 import { useMouse } from '../composables/mouse'
 
 export default defineComponent({
+  // nombre del componente
   name:'FullComponent',
+  // porps que recibe el componente
   props:{
     texto: String,
     numero: Number
   },
+  // datos reactivos del componente (debe ser una función que devuelve un objeto)
   data(){
     return{
       dato: 123,
@@ -38,6 +41,7 @@ export default defineComponent({
       nombre: ''
     }
   },
+  // métodos: son funciones disponibles en el scope del componente
   methods:{
     obtenerUsuario(): string{
       return this.usuario;
@@ -55,18 +59,23 @@ export default defineComponent({
     cambioProfundo(){
       this.datos.nombre = 'otro nombre'
       this.setTexto2('Este es el nuevo texto!!')
-      //this.texto2 = 'sdfsdfsdf' // Incorrecto!!
+      //this.texto2 = 'sdfsdfsdf' //  ̶I̶n̶c̶o̶r̶r̶e̶c̶t̶o̶!̶!̶  SÍ es correcto, pero solo posible dentro del scope del componente
     }
   },
+  // funciones que no alteran el estado del componente, 
+  // generalmente utilizadas en el frontend
   computed:{
     nombreCompleto():string{
       return this.datos.nombre + ' ' + this.datos.apellido
     }
   },
+  // array con la lista de eventos que dispara el componente
   emits:['modificar-padre', 'boton-pulsado'],
+  // componentes registrados para ser utilizados dentro de éste
   components:{
 
   },
+  // vigila el cambio en el valor de una variable
   watch:{
     nombre(nuevo:string, prev:string){
       if(nuevo=='Juan'){
@@ -82,17 +91,30 @@ export default defineComponent({
   mounted(){
     //alert('Bienvenido')
   },
-  // updated, created, unmounted
-  // beforeMounted, beforeUpdated,  beforeCreated, beforeUnmounted
+  // beforeCreate, created
+  // beforeMounted, mounted
+  // beforeUpdate, updated
+  // beforeUnmount, unmounted
+
+  // Composition API sólo se ejecuta una vez y define composable functions para permitir la reactividad
   setup(){
     const mouse = useMouse()
 
+    // ref para valores primitivos (string, number, boolean)
     const texto2 = ref('Mi texto!!!')
 
+    // reactive para valores complejos dentro de un object
+    const obj = reactive({
+      a:1,
+      b:2
+    })
+
+    // setters para compartir el método de modificación del ref
     const setTexto2 = (nuevoTexto:string) => {
       texto2.value = nuevoTexto
     }
 
+    // al final del setup compartimos el conjunto de variables / métodos que necesita el componente
     return {
       texto2,
       setTexto2,
