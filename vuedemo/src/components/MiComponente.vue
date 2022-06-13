c05dad6909905f598b5d249c9c6c2d2f72198d9c
Juan Searle (Casa) <jsearle@gmail.com>
2022-06-13 10:53:02 +0200

    MiComponente

---
+++ vuedemo/src/components/MiComponente.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Mi componente</h1>
+  <p>{{texto}}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MiComponente',
+  data(){
+    return {
+      texto: "Este texto es reactivo"
+    }
+  }
+});
+</script>
+
+<style>
+p{
+  color:red;
+}
+</style>
