acf7fa5614d421d6a29aaa561f41fce7c57864fe
Juan Searle (Casa) <jsearle@gmail.com>
2022-06-16 15:16:40 +0200

    Reactive Map!

---
+++ playground/src/composables/map.ts
@@ -0,0 +1,68 @@
+import { onMounted, reactive, ref }  from 'vue'
+import { Loader } from '@googlemaps/js-api-loader';
+
+
+export function useMaps(){
+  let google:any, map:any;
+  const mapCenter = reactive({coords: {lat: 40.352784, lng: -3.70061}, zoom: 15});
+  const isLoaded = ref(false)
+  const markers = reactive([] as any[]);
+
+
+  const createMarker = (position:any)=>{
+    const markerOptions: any = {
+      map: map,
+      position,
+      icon : {
+        path: 'M18 2c1.1 0 2 0.9 2 2l0 13c0 1.1-0.9 2-2 2l-4 0 -3 3 -3-3 -4 0c-1.1 0-2-0.9-2-2l0-13c0-1.1 0.9-2 2-2l14 0Zm-7 10.9l2.8 1.7 -0.7-3.2 2.5-2.1 -3.2-0.3 -1.3-3 -1.3 3 -3.2 0.3 2.5 2.1 -0.7 3.2 2.8-1.7Z',
+        scale: 1.8,
+        anchor: new google.maps.Point(11, 22),
+        fillOpacity: 1,
+        fillColor: '#c14e34',
+        strokeOpacity: 0
+      }
+    }
+    const newMarker = new google.maps.Marker(markerOptions)
+    markers.push(newMarker);
+  }
+
+  const createMap = (element:any) =>{
+    console.log(element, google, mapCenter.coords)
+    if (google == undefined) return;
+    const opts:any = {
+      center: mapCenter.coords,
+      zoom: mapCenter.zoom,
+      styles: [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"},{"color":"#f49f53"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f9ddc5"},{"lightness":-7}]},{"featureType":"poi.business","elementType":"all","stylers":[{"color":"#645c20"},{"lightness":38}]},{"featureType":"poi.government","elementType":"all","stylers":[{"color":"#9e5916"},{"lightness":46}]},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"color":"#813033"},{"lightness":38},{"visibility":"off"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"color":"#645c20"},{"lightness":39}]},{"featureType":"poi.school","elementType":"all","stylers":[{"color":"#a95521"},{"lightness":35}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"color":"#9e5916"},{"lightness":32}]},{"featureType":"road","elementType":"all","stylers":[{"color":"#813033"},{"lightness":43}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#f19f53"},{"weight":1.3},{"visibility":"on"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#f19f53"},{"lightness":-10}]},{"featureType":"transit","elementType":"all","stylers":[{"lightness":38}]},{"featureType":"transit.line","elementType":"all","stylers":[{"color":"#813033"},{"lightness":22}]},{"featureType":"transit.station","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#1994bf"},{"saturation":-69},{"gamma":0.99},{"lightness":43}]}],
+      maxZoom: 20,
+      minZoom: 0,
+      mapTypeId: 'terrain',
+      clickableIcons: true,
+      disableDoubleClickZoom: false,
+      draggable: true,
+      keyboardShortcuts: true,
+      scrollwheel: true,
+    };
+    console.log(opts)
+    map = new google.maps.Map(element, opts);
+    console.log(map)
+  }
+
+  onMounted( async ()=>{
+    const loader = new Loader({
+      apiKey: 'AIzaSyDKtd8L97X6mEGTEIqyuZzj3m1mBNDAlgk',
+      version: '3.31',
+      libraries: ['places','geometry']
+    });
+    google = await loader.load()
+    isLoaded.value = true;
+  })
+
+  return {
+    createMap,
+    createMarker,
+    mapCenter,
+    markers,
+    isLoaded
+  }
+
+}
--- playground/src/pages/MapsPage.vue
+++ playground/src/pages/MapsPage.vue
@@ -4,11 +4,18 @@
       <ion-toolbar>
         <ion-buttons slot="start"
           ><ion-menu-button></ion-menu-button></ion-buttons
-        ><ion-title>Mapa iframe</ion-title>
+        ><ion-title>Mapa iframe + nativo</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <iframe src="../assets/map.html" width="100%" height="500px" style="border:none;"></iframe>
+      <ion-card class="ion-padding" v-show="map.isLoaded.value">
+        <h2>Mapa reactivo</h2>
+        <div ref="reactiveMap" id="map"></div>
+      </ion-card>
+      <ion-card class="ion-padding">
+        <h2>Mapa incrustado por iFrame</h2>
+        <iframe src="../assets/map.html" width="100%" height="300px" style="border:none;"></iframe>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
@@ -30,9 +37,10 @@ import {
 } from "@ionic/vue";
 import { defineComponent } from "vue";
 import { Capacitor } from "@capacitor/core";
+import { useMaps } from "../composables/map";
 
 export default defineComponent({
-  name: "CapacitorPage",
+  name: "MapsPage",
   components: {
     IonPage,
     IonHeader,
@@ -42,14 +50,30 @@ export default defineComponent({
     IonButtons,
     IonMenuButton
   },
+  mounted(){
+    console.log("Creando mapa...");
+  },
+  watch:{
+    'map.isLoaded.value'(newVal: any, oldVal: any){
+      console.log("Mapa cargado!", newVal);
+      this.map.createMap( this.$refs.reactiveMap )
+    }
+  },
   setup() {
-    console.log("setup");
     const platform = Capacitor.getPlatform();
     const native = Capacitor.isNativePlatform();
+    const map = useMaps()
     return {
       platform,
       native,
+      map
     };
   },
 });
 </script>
+<style scoped>
+#map {
+  height: 400px;
+  width: 100%;
+}
+</style>
